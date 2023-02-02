f9dfb2066fbd487d95ce9b26c38594143f72b681
Anshid <anshid.ansar@gmail.com>
2023-02-02 11:31:27 +0530

    item added

---
+++ FoodSpot/BLL/ITEMS/Property/CategoryProperty.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Food_Mgmt_System
+{
+    public class CategoryProperty
+    {
+        public int Cat_Id { get; set; }
+
+        public string Cat_Name { get; set; }
+
+        public string Cat_Status { get; set; }
+    }
+}
