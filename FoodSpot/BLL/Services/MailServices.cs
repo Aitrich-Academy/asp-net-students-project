c6c904998de70071dac272a87ddca318025e4ca9
Rekha <rekharajankr@gmail.com>
2023-02-08 16:00:23 +0530

    Mail services

---
+++ FoodSpot/BLL/Services/MailServices.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    internal class MailServices
+    {
+        public void MailSending(string mailid, AlternateView av)
+        {
+            using (MailMessage mail = new MailMessage())
+            {
+                mail.From = new MailAddress("anshid.ansar@gmail.com");
+                mail.To.Add(mailid);
+                mail.Subject = "Food Spot";
+
+                mail.AlternateViews.Add(av);
+
+                mail.IsBodyHtml = true;
+
+                using (SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587))
+                {
+                    smtp.Credentials = new NetworkCredential("anshidansar007", "ngvrpntimlvnmgik");
+                    smtp.EnableSsl = true;
+                    smtp.Send(mail);
+                }
+
+            }
+        }
+    }
+}
