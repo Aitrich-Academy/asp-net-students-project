c623499e2fe1c5ec93229184cb5e5074bc9d3e2f
Pranav <memyselfpranavkrishna@gmail.com>
2023-02-15 16:08:17 +0530

    Landing Page 15th fbry

---
+++ FoodSpot/BLL/MenuManager.cs
@@ -0,0 +1,53 @@
+﻿using BLL.Admin.Property;
+using BLL.CATEGORY.Property;
+using BLL.Property;
+using project;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    
+    
+    public class MenuManager
+
+    {
+        private DBhelper dbh = new DBhelper();
+        public CategoryProperty prop = new CategoryProperty();
+        public ItemProperty itemProp_Obj = new ItemProperty();
+        private SortedList S1 = new SortedList();
+        public RestaurantProperty proper = new RestaurantProperty();
+        public BookingProperty bprop = new BookingProperty();
+        public LoginProperty logpro = new LoginProperty();
+        
+        public DataTable Displaymenu()
+        {
+            DataTable dt = new DataTable();
+            dt = dbh.getdatatable("displaymenu");
+            return dt;
+        }
+        public DataTable displaymenu()
+        {
+            S1.Clear();
+            S1.Add("Rest_Id", proper.ID);
+            DataTable dt= dbh.getdatatable(S1, "displaymenu");
+            return dt;
+        }
+
+        public string bookingquery(string s)
+        {
+            logpro.log_Id = int.Parse(s);
+            S1.Clear();
+            S1.Add("Cust_Id", logpro.log_Id) ;
+            S1.Add("Item_Id", itemProp_Obj.Item_Id);
+            S1.Add("Book_Quantity", bprop.Book_Quantity);
+            return dbh.executeprocedure(S1, "bookingquery");
+        }
+        
+    }
+}
---
+++ FoodSpot/start.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using project;
+using BLL.Manager;
+
+namespace FoodSpot
+{
+    
+    public partial class start : System.Web.UI.Page
+    {
+        StartManager srt = new StartManager();
+
+        public object HiddenField1 { get; private set; }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                if (Session["Username"] != null)
+                {
+                    
+                    Label1.Visible = true;
+                    Label1.Text = "Welcome "+Session["Username"].ToString();
+                }
+                    DataList1.DataSource = srt.ListAllRestaurants();
+                DataList1.DataBind();
+            }
+        }
+
+
+
+        protected void LinkButton1_Click(object sender, EventArgs e)
+        {
+            var button = sender as LinkButton;
+            var dataListItem = button.Parent as DataListItem;
+            var hfield = dataListItem.FindControl("hfield") as HiddenField;
+            string s1 = hfield.Value;
+            srt.prop.ID = int.Parse(s1);
+            Response.Redirect("menu.aspx?id=" + s1);
+        }
+
+        
+    }
+}
