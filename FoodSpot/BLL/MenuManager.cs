a253d4377a863407cc7a9902461d83206f64da1e
Pranav <memyselfpranavkrishna@gmail.com>
2023-03-10 15:39:07 +0530

    as

--- FoodSpot/BLL/MenuManager.cs
+++ FoodSpot/BLL/MenuManager.cs
@@ -1,5 +1,5 @@
 ﻿using BLL.Admin.Property;
-using BLL.CATEGORY.Property;
+using BLL.ADMIN.PROPERTY;
 using BLL.Property;
 using project;
 using System;
@@ -21,7 +21,7 @@ namespace BLL
         public CategoryProperty prop = new CategoryProperty();
         public ItemProperty itemProp_Obj = new ItemProperty();
         private SortedList S1 = new SortedList();
-        public RestaurantProperty proper = new RestaurantProperty();
+        public ADMIN.PROPERTY.RestaurantProperty proper = new ADMIN.PROPERTY.RestaurantProperty();
         public BookingProperty bprop = new BookingProperty();
         public LoginProperty logpro = new LoginProperty();
         
@@ -31,22 +31,23 @@ namespace BLL
             dt = dbh.getdatatable("displaymenu");
             return dt;
         }
-        public DataTable displaymenu()
+        public DataSet displaymenu1()
         {
             S1.Clear();
-            S1.Add("Rest_Id", proper.ID);
-            DataTable dt= dbh.getdatatable(S1, "displaymenu");
+            S1.Add("restId", proper.ID);
+            DataSet dt = dbh.getdataset(S1, "ListItems");
             return dt;
         }
 
-        public string bookingquery(string s)
+        public string bookingquery(string s,string restid)
         {
             logpro.log_Id = int.Parse(s);
             S1.Clear();
+            S1.Add("Rest_Id", restid);
             S1.Add("Cust_Id", logpro.log_Id) ;
             S1.Add("Item_Id", itemProp_Obj.Item_Id);
             S1.Add("Book_Quantity", bprop.Book_Quantity);
-            return dbh.executeprocedure(S1, "bookingquery");
+            return dbh.executeprocedure(S1,"bookingquery");
         }
         
     }
